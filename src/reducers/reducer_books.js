0f3d5c25ee196b9b161b73e96f84b1f9083b1fd0
SarahPAms <Sarah.L.Polan@gmail.com>
2017-04-16 19:46:20 +0200

    udemy tutorial project simple redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+
+export default function() {
+  return [
+    { title: 'Javascript', pages: 372},
+    { title: 'Harry Potter', pages: 39},
+    { title: 'The Dark Tower', pages: 1000},
+    { title: 'Eloquent Ruby', pages: 1}
+  ]
+}
